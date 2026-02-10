b95660cdd6022406eefec44f859e0767d9a4d996
Gustavo Ab. <133971751+GustavoAb18@users.noreply.github.com>
2026-02-10 15:04:43 -0300

    Add files via upload

---
+++ exercicio1.js
@@ -0,0 +1,5 @@
+// Usamos const para criar uma variável fixa.
+const nome = "GitHub";
+
+// Por fim, colocamos 'console.log' para mostrar na tela.
+console.log("Olá, " + nome + "!");
---
+++ exercicio2.js
@@ -0,0 +1,12 @@
+/*
+ * Usamos dois valores distintos com o 'let', que é uma variável que muda sempre. 
+ * Então um outro, sendo o resultado. Nele faremos soma de = + , - , * , /
+ * Por fim, demonstramos na tela o resultado de fato desses dois valores.
+*/
+
+let valor1 = 5;
+let valor2 = 25;
+
+let resultado = valor1 + valor2;
+
+console.log(resultado);
